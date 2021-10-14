1ef83902c3f729c45925caac6ba6b5d0cc1e0281
wangshuai <bjwangs@163.com>
2021-10-14 10:29:56 +0800

    FirstCommit!

---
+++ app/src/main/java/cn/shineiot/wancompose/MainActivity.kt
@@ -0,0 +1,38 @@
+package cn.shineiot.wancompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import cn.shineiot.wancompose.ui.theme.WanComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WanComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WanComposeTheme {
+        Greeting("Android")
+    }
+}
