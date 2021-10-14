76ffb4dfa59c23286ac61bd82d4e35c5440d763e
wangshuai <bjwangs@163.com>
2021-10-14 12:00:19 +0800

    ..

---
+++ app/src/main/java/cn/shineiot/wancompose/ui/fragments/HomeFragment.kt
@@ -0,0 +1,21 @@
+package cn.shineiot.wancompose.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
