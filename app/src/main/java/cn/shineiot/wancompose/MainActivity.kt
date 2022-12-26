8f7f0801d88866ff4cb368af74e8264332ef35d2
wangshuai <bjwangs@163.com>
2022-12-26 14:50:05 +0800

    了解布局

--- app/src/main/java/cn/shineiot/wancompose/MainActivity.kt
+++ app/src/main/java/cn/shineiot/wancompose/MainActivity.kt
@@ -3,11 +3,22 @@ package cn.shineiot.wancompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import cn.shineiot.wancompose.ui.bean.Message
 import cn.shineiot.wancompose.ui.theme.WanComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +28,7 @@ class MainActivity : ComponentActivity() {
             WanComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting(Message("JAVA", "Kotlin"))
                 }
             }
         }
@@ -25,14 +36,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(msg: Message) {
+    Row(modifier = Modifier.padding(5.dp)) {
+        Image(
+            painter = painterResource(R.drawable.huojiang),
+            contentDescription = "image",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column() {
+            Text(text = msg.msg, modifier = Modifier
+                .width(100.dp)
+                .background(color = Color(R.color.cardview_dark_background)))
+            Text(text = msg.title)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     WanComposeTheme {
-        Greeting("Android")
+        Greeting(Message("java", "kotlin"))
     }
 }
---
+++ app/src/main/java/cn/shineiot/wancompose/ui/bean/Message.kt
@@ -0,0 +1,11 @@
+package cn.shineiot.wancompose.ui.bean
+
+/**
+ * @Description
+ * @Author : GF63
+ * @Date : 2022/12/26
+ */
+data class Message(
+    var msg : String,
+    var title : String
+)
