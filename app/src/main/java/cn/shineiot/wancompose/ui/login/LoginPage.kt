c14bc93c2fcb355581b488dfa2320cf992869993
wangshuai <bjwangs@163.com>
2022-12-29 10:10:28 +0800

    status

--- app/src/main/java/cn/shineiot/wancompose/ui/login/LoginPage.kt
+++ app/src/main/java/cn/shineiot/wancompose/ui/login/LoginPage.kt
@@ -1,19 +1,56 @@
 package cn.shineiot.wancompose.ui.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import cn.shineiot.wancompose.RouteConfig
 import cn.shineiot.wancompose.route.NavUtil
+import cn.shineiot.wancompose.ui.theme.F2
+import cn.shineiot.wancompose.ui.theme.dp10
+import cn.shineiot.wancompose.ui.theme.dp20
+import cn.shineiot.wancompose.ui.theme.dp30
 
 /**
  * login
  */
+@Preview
 @Composable
 fun LoginPage() {
-    Button(onClick = { NavUtil.get().navigation(RouteConfig.ROUTE_MAIN) }) {
-        Text(text = "Login")
+    Column(
+        modifier = Modifier
+            .fillMaxSize(1f)
+            .background(F2)
+            .padding(start = dp10, end = dp20)
+    ) {
+
+        var username by remember {
+            mutableStateOf("请输入用户名")
+        }
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = {},
+            placeholder = {},
+            label = {}
+        )
+        Button(
+            onClick = { NavUtil.get().navigation(RouteConfig.ROUTE_MAIN) },
+            Modifier.padding(5.dp)
+        ) {
+            Text(
+                text = "Login",
+            )
+        }
     }
 }
 
---
+++ app/src/main/java/cn/shineiot/wancompose/ui/theme/Dimens.kt
@@ -0,0 +1,34 @@
+package cn.shineiot.wancompose.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * @Description
+ * @Author : GF63
+ * @Date : 2022/12/29
+ */
+
+val dp1 = 1.dp
+val dp5 = 5.dp
+val dp8 = 8.dp
+val dp10 = 10.dp
+val dp15 = 15.dp
+val dp20 = 20.dp
+val dp25 = 25.dp
+val dp30 = 30.dp
+val dp40 = 40.dp
+val dp50 = 50.dp
+val dp60 = 60.dp
+val dp70 = 70.dp
+val dp80 = 80.dp
+val dp100 = 100.dp
+
+
+val sp12 = 12.sp
+val sp13 = 13.sp
+val sp14 = 14.sp
+val sp15 = 15.sp
+val sp16 = 16.sp
+val sp18 = 18.sp
+val sp22 = 22.sp
